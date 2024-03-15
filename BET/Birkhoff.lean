17a513550f45e0624f9e7f0154d1c09a87c58088
Marcello Seri <marcello.seri@gmail.com>
2024-03-15 11:45:21 +0100

    Birkhoff: add extra proofs for maxOfSums_succ_image
    Signed-off-by: Marcello Seri <marcello.seri@gmail.com>

--- BET/Birkhoff.lean
+++ BET/Birkhoff.lean
@@ -30,15 +30,18 @@ open BigOperators MeasureTheory
 variable {α : Type*} [MeasurableSpace α]
 variable {μ : MeasureTheory.Measure α} [MeasureTheory.IsProbabilityMeasure μ]
 variable (T : α → α) (hT : MeasurePreserving T μ)
-variable (f g : α → ℝ) (hf : Integrable f μ) (hg : Integrable g μ)
+variable (f g φ : α → ℝ) (hf : Integrable f μ) (hg : Integrable g μ)
 
 
 open Finset in
 /-- The max of the first `n + 1` Birkhoff sums.
 I.e., `maxOfSums T f x n` corresponds to `max {birkhoffSum T f 1 x,..., birkhoffSum T f (n + 1) x}`.
 Indexing choice avoids max of empty set issue. -/
+/- define `Φ_n : max { ∑_{i=0}^{n} φ ∘ T^i }_{k ≤ n}` -/
 def maxOfSums (x : α) (n : ℕ) :=
     sup' (range (n + 1)) (nonempty_range_succ) (fun k ↦ birkhoffSum T f (k + 1) x)
+
+/- Note that maxOfSums T φ x n corresponds to Φ_{n+1} -/
 
 theorem maxOfSums_zero : maxOfSums T f x 0 = f x := by
   unfold maxOfSums
@@ -65,36 +68,141 @@ theorem maxOfSums_le_le' (x : α) : Monotone (fun n ↦ maxOfSums T f x n) := by
   intros n m hmn
   exact maxOfSums_le_le T f x n m hmn
 
+
 open Filter in
-/-- The set of divergent points `{ x | sup_n ∑_{i=0}^{n} f(T^i x) = ∞}`. -/
-def divSet := { x : α | Tendsto (fun n ↦ maxOfSums T f x n) atTop atTop }
+/-- The set of divergent points `{ x | lim_n Φ_n x = ∞}`. -/
+def divSet := { x : α | Tendsto (fun n ↦ maxOfSums T φ x n) atTop atTop }
+
+
+/- ∀ `x ∈ A`, `Φ_{n+1}(x) - Φ_{n}(T(x)) = φ(x) - min(0,Φ_{n}(T(x))) ≥ φ(x)` decreases to `φ(x)`. -/
+
+/-- Convenient combination of `birkhoffSum` terms. -/
+theorem birkhoffSum_succ_image (n : ℕ) (x : α) :
+      birkhoffSum T f n (T x) = birkhoffSum T f (n + 1) x - f x := by
+    rw [birkhoffSum_add T f n 1 x]
+    rw [eq_add_of_sub_eq' (birkhoffSum_apply_sub_birkhoffSum T f n x)]
+    simp
+    exact add_sub (birkhoffSum T f n x) (f (T^[n] x)) (f x)
+
+-- TODO: clean the following, enquire if there is an easier way to extract it from mathlib.
+/-- Would expect this to be in `Mathlib/Data/Finset/Lattice`. -/
+theorem sup'_eq_iff_le {s : Finset β} [SemilatticeSup α] (H : s.Nonempty) (f : β → α) (hs : a ∈ s) :
+    s.sup' H f = f a ↔ ∀ b ∈ s, f b ≤ f a := by
+  constructor
+  intros h0 b h2
+  rw [← h0]
+  exact Finset.le_sup' f h2
+  intro h1
+  have hle : s.sup' H f ≤ f a := by
+    simp only [Finset.sup'_le_iff]
+    exact h1
+  exact (LE.le.ge_iff_eq hle).mp (Finset.le_sup' f hs)
+
+
+open Finset in
+/-- Claim 1 (Marco) -/
+theorem maxOfSums_succ_image (n : ℕ) (x : α) :
+    maxOfSums T φ x (n + 1) - maxOfSums T φ (T x) n = φ x - min 0 (maxOfSums T φ (T x) n) := by
+  -- Consider `maxOfSums T f x (n + 1) = max {birkhoffSum T f 1 x,..., birkhoffSum T f (n + 2) x}`
+  by_cases hc : ∀ k ≤ n + 1, birkhoffSum T f (k + 1) x ≤ birkhoffSum T f 1 x
+  -- Case when the max is achieved by first element.
+  have h0 : maxOfSums T f x (n + 1) = birkhoffSum T f 1 x := by
+    unfold maxOfSums
+    rw [birkhoffSum_one']
+    rw [birkhoffSum_one'] at hc
+    have h12 : (range (n + 1 + 1)).Nonempty := nonempty_range_succ
+    have h13 : 0 ∈ (range (n + 1 + 1)) := by
+      refine mem_range.mpr (Nat.add_pos_right (n + 1) Nat.le.refl)
+    have h11 := sup'_eq_iff_le h12 (fun k ↦ birkhoffSum T f (k + 1) x) h13
+    simp only [zero_add, birkhoffSum_one', mem_range] at h11
+    have h15 : ∀ b < n + 1 + 1, birkhoffSum T f (b + 1) x ≤ f x := by
+      intros k h16
+      have h17 : k ≤ n + 1 := Nat.lt_succ.mp h16
+      exact hc k h17
+    exact h11.mpr h15
+  have h1 : birkhoffSum T f 1 x = f x := birkhoffSum_one T f x
+  have h2 : ∀ k, birkhoffSum T f k (T x) = birkhoffSum T f (k + 1) x - f x := by
+    intro k
+    exact birkhoffSum_succ_image T f k x
+  have h3 : ∀ k ≤ n + 1, birkhoffSum T f k (T x) ≤ 0 := by
+    intros k hk
+    rw [h2]
+    rw [h1] at hc
+    simp only [tsub_le_iff_right, zero_add]
+    exact hc k hk
+  have h4 : maxOfSums T f (T x) n ≤ 0 := by
+    unfold maxOfSums
+    simp only [sup'_le_iff, mem_range]
+    intros k hk
+    rw [Nat.lt_succ] at hk
+    refine h3 (k + 1) (Nat.add_le_add hk Nat.le.refl)
+  have h5 : min 0 (maxOfSums T f (T x) n) = maxOfSums T f (T x) n := min_eq_right h4
+  linarith
+  -- Case when max is not achieved by the first element
+  push_neg at hc
+  let bS := fun k ↦ birkhoffSum T f (k + 1) x
+  let s1 := range 1
+  let s2 := filter (1 ≤ ·) (range (n + 2))
+  have h19 : range (n + 2) = s1 ∪ s2 := by
+    have h20 := range_union 1 (n + 1)
+    rw [Nat.one_add (n + 1)] at h20
+    exact h20
+  have h21 : s2.Nonempty := by
+    simp
+    use 1
+
+    sorry
+  have h17 : sup' (range (n + 2)) nonempty_range_succ bS = bS 0 ⊔ sup' s2 h21 bS := by
+    -- Now use `sup'_union`, `range_union` to divide `maxOfSums T f x (n + 1)` into 2 pieces.
+
+    sorry
+
+  have h6 : maxOfSums T f x (n + 1) =
+      sup' (range (n + 1)) (nonempty_range_succ) (fun k ↦ birkhoffSum T f (k + 2) x) := by
+    unfold maxOfSums
+    -- Since `hc`, the max is not achieved by the first element reduce to max over other terms.
+
+    sorry
+  have h7 : maxOfSums T f (T x) n =
+      sup' (range (n + 1)) (nonempty_range_succ) (fun k ↦ birkhoffSum T f (k + 1) (T x)) := by
+    exact rfl
+  have h10 : maxOfSums T f x (n + 1) - maxOfSums T f (T x) n = f x := by
+    rw [h6, h7]
+    have h18 (k : ℕ) := birkhoffSum_succ_image T f (k + 1) x
+    have h19 :
+        (fun k ↦ birkhoffSum T f (k + 2) x) = (fun k ↦ birkhoffSum T f (k + 1) (T x) + f x) := by
+
+      sorry
+    rw [h19]
+
+    -- Use `sup'_comm` to allow sup to commute with the function.
+    sorry
+  have h8 : 0 ≤ maxOfSums T f (T x) n := by
+    unfold maxOfSums
+
+    sorry
+  have h9 : min 0 (maxOfSums T f (T x) n) = 0 := by
+    exact min_eq_left h8
+  rw [h9, h6, h7]
+
+  sorry
+  sorry
 
 
 
+open Filter in
+/-- The set of divergent points is invariant. -/
+theorem divSet_inv : T⁻¹' (divSet T f) = (divSet T f) := by
+  sorry
 
 
 
-
-
-
-
-
-
-/- define `A := { x | sup_n ∑_{i=0}^{n} f(T^i x) = ∞}`.
-def A := { x : α | ∃ n, ∀ C : ℝ, ∑ i in Finset.range n, f (T^[i] x) > C }
-
-define `A := { x | lim_n ∑_{i=0}^{n} f(T^i x) = ∞}`.
--/
-def A := { x : α | Filter.Tendsto (fun n ↦ ∑ i in Finset.range n, f (T^[i] x)) Filter.atTop Filter.atTop }
 
 /- `A` is in `I = inv_sigma_algebra`. -/
 -- idea: is it better to define a new type measureable sets in alpha and then restrict to that type?
 -- def inv_sigma_algebra := { S : Set α | MeasurableSet S ∧ T⁻¹' S = S }
 def inv_sigma_algebra := { S : Set α | MeasurableSet S ∧ IsInvariant (fun n x ↦ T^[n] x) S }
 
-/- define `Φ_n : max { ∑_{i=0}^{n} φ ∘ T^i }_{k ≤ n}` -/
-
-/- ∀ `x ∈ A`, `Φ_{n+1}(x) - Φ_{n}(T(x)) = φ(x) - min(0,Φ_{n}(T(x))) ≥ φ(x)` decreases to `φ(x)`. -/
 
 /- Using dominated convergence, `0 ≤ ∫_A (Φ_{n+1} - Φ_{n}) dμ = ∫_A (Φ_{n+1} - Φ_{n} ∘ T) dμ → ∫_A φ dμ`. -/
 

a2178b29028cc21719d409b054242eb98c5daeec
Marco Lenci <peonzio@yahoo.com>
2024-03-27 17:20:42 +0100

    more work and few (easy) sorries

--- BET/marcossandbox.lean
+++ BET/marcossandbox.lean
@@ -1,3 +1,5 @@
+import Mathlib
+
 noncomputable section
 open MeasureTheory
 
@@ -5,6 +7,9 @@ variable {α : Type*} [m0: MeasurableSpace α]
 variable {μ : Measure α} [IsProbabilityMeasure μ]
 variable {T : α → α} (hT : Measurable T)
 variable {f : α → ℝ} (hf : Integrable f μ)
+
+lemma my_count_union (A B: ℕ → Set α) (h : ∀ i, A i = B i) : ⋃ i, A i = ⋃ i, B i := by sorry
+-- the above MUST be in mathlib, but I couldn't find it!
 
 def invSigmaAlg (hT : Measurable T) : MeasurableSpace α where
   MeasurableSet' s := MeasurableSet s ∧ T ⁻¹' s = s
@@ -15,13 +20,21 @@ def invSigmaAlg (hT : Measurable T) : MeasurableSpace α where
   measurableSet_compl := by
     intro s
     dsimp
-    intro hinit -- the lest 3 line can be abbreviated to: 'intro h hinit'
-    obtain ⟨hinit1, hinti2⟩ := hinit
+    intro hinit -- the lest 3 lines can be abbreviated to: 'intro h hinit'
+    obtain ⟨hinit1, hinit2⟩ := hinit
     constructor
     · exact MeasurableSet.compl hinit1
-    · rw [Set.preimage_compl]
-      exact congrArg compl hinti2
+    · exact congrArg compl hinit2 -- this was suggested by Lean
+      -- problems at least with infoview on this part
   measurableSet_iUnion := by
-    intro seq
+    intro s
     dsimp
-    sorry
+    intro hinit
+    constructor
+    · have hi1st : ∀ i, MeasurableSet (s i) := by
+        intro i
+        exact (hinit i).left
+      exact MeasurableSet.iUnion hi1st
+    · have hi2nd : ∀ i, T ⁻¹'(s i) = s i := by sorry
+      rw [Set.preimage_iUnion]
+      exact my_count_union (fun i ↦ T ⁻¹' s i) (fun i ↦ s i) hi2nd

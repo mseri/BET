798d8adcceda16d1a083a2489b94ab2da19f8556
Marco Lenci <peonzio@yahoo.com>
2024-03-28 12:05:09 +0100

    lemma leq_InvSigmaAlg_FullAlg + other improvements

--- BET/marcossandbox.lean
+++ BET/marcossandbox.lean
@@ -1,7 +1,72 @@
-import Mathlib
+import Mathlib.Tactic
+import Mathlib.MeasureTheory.Function.ConditionalExpectation.Basic
+
 
 noncomputable section
+open MeasureTheory
 
+variable {α : Type*} [m0: MeasurableSpace α]
+variable {μ : Measure α} [IsProbabilityMeasure μ]
+variable {T : α → α} (hT: Measurable T)
+-- variable (hT : Measurable T)
+variable {f φ : α → ℝ}
+
+def invSigmaAlg_test (hT : Measurable T) : MeasurableSpace α where
+  MeasurableSet' := fun s ↦ MeasurableSet s ∧ T ⁻¹' s = s
+  measurableSet_empty := by
+    constructor
+    · exact MeasurableSet.empty
+    · exact rfl
+  measurableSet_compl := sorry
+  measurableSet_iUnion := sorry
+
+lemma invSigmaAlg_le : invSigmaAlg hT ≤ m0 := sorry
+
+lemma my_lemma2 (s : Set α) (hs : MeasurableSet s) :
+  MeasurableSet sᶜ := MeasurableSet.compl hs -- also 'hs.compl'
+
+lemma my_lemma (s : Set α) (hs : MeasurableSet[invSigmaAlg hT] s) :
+  MeasurableSet[invSigmaAlg hT] sᶜ := MeasurableSet.compl hs -- also 'hs.compl'
+
+example : α → ℝ := condexp (invSigmaAlg hT) μ f
+
+end
+
+--- my sandbox
+
+section
+
+variable (s t u : Set α)
+open Set
+
+example (h : s ⊆ t) : s ∩ u ⊆ t ∩ u := by
+  rw [subset_def, inter_def, inter_def]
+  rw [subset_def] at h
+  simp only [mem_setOf]
+  rintro x ⟨xs, xu⟩
+  exact ⟨h _ xs, xu⟩
+
+example (h : s ⊆ t) : s ∩ u ⊆ t ∩ u := by
+  simp only [subset_def, mem_inter_iff] at *
+  rintro x ⟨xs, xu⟩
+  exact ⟨h _ xs, xu⟩
+
+example : s ∩ (t ∪ u) ⊆ s ∩ t ∪ s ∩ u := by
+  intro x hx
+  have xs : x ∈ s := hx.1
+  have xtu : x ∈ t ∪ u := hx.2
+  rcases xtu with xt | xu
+  · left
+    show x ∈ s ∩ t
+    exact ⟨xs, xt⟩
+  . right
+    show x ∈ s ∩ u
+    exact ⟨xs, xu⟩
+
+end
+
+
+noncomputable section
 open MeasureTheory
 
 variable {α : Type*} [m0: MeasurableSpace α]
@@ -38,13 +103,12 @@ def invSigmaAlg : MeasurableSpace α where
     · exact rfl
   measurableSet_compl := by
     intro s
-    dsimp
+    dsimp only -- problems at least with infoview on this part
     intro hinit -- the last 3 lines can be abbreviated to: 'intro h hinit'
     obtain ⟨hinit1, hinit2⟩ := hinit
     constructor
     · exact MeasurableSet.compl hinit1
     · exact congrArg compl hinit2 -- this was suggested by Lean
-      -- problems at least with infoview on this part
   measurableSet_iUnion := by
     intro s
     dsimp
@@ -61,6 +125,17 @@ def invSigmaAlg : MeasurableSpace α where
       exact my_count_union (fun i ↦ T ⁻¹' s i) (fun i ↦ s i) hi2nd
     done
 
+/- the lemma below was a problem because it was hard to find out what m ≤ m0 meant, when
+m, m0 are measurable spaces. Hovering over the ≤ sugn in infoview and following links
+explained it:
+instance : LE (MeasurableSpace α) where le m₁ m₂ := ∀ s, MeasurableSet[m₁] s → MeasurableSet[m₂] s
+-/
+lemma leq_InvSigmaAlg_FullAlg : invSigmaAlg T ≤ m0 := by
+  intro s hs -- here the infoview is faulty and doesn't show everything (known Lean problem, Floris says)
+  exact hs.left
+  done
+
+-------------------------------------
 /- the following is an earlier version before I realized we never needed that T is
 measurable for this definition -/
 def invSigmaAlg_previous (hT : Measurable T) : MeasurableSpace α where

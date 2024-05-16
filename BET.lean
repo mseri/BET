7e02afa68479960236607254226bb7361a3719ec
Oliver Butterley <oliver.butterley@gmail.com>
2024-05-16 20:50:23 +0200

    extract minimal set stuff to separate file

--- BET.lean
+++ BET.lean
@@ -1,4 +1,5 @@
 -- This module serves as the root of the `BET` library.
 -- Import modules here that should be built as part of the library.
 import «BET».Birkhoff
+import «BET».Minimal
 import «BET».Topological
--- BET/Topological.lean
+++ BET/Topological.lean
@@ -11,12 +11,9 @@ import Mathlib.Dynamics.Ergodic.AddCircle
 /-!
 # Topological dynamics
 
-This file defines Birkhoff sums, other related notions and proves Birkhoff's ergodic theorem.
+...
 
 ## Implementation notes
-
-We could do everything in a topological space, using filters and neighborhoods, but it will
-be more comfortable with a metric space.
 
 TODO: at some point translate to topological spaces
 
@@ -259,66 +256,8 @@ theorem periodicpts_mem_recurrentSet (x : α) (n : ℕ) (nnz : n ≠ 0) (hx : Is
 theorem recurrentSet_nonwandering : recurrentSet f ⊆ (nonWanderingSet f) :=
   fun _ ↦ fun hz ↦ omegaLimit_nonwandering _ _ (mem_setOf_eq ▸ hz)
 
-/-- The minimal subsets are the closed invariant subsets in which all orbits are dense. -/
-structure IsMinimalSubset (f : α → α) (U : Set α) : Prop :=
-  (closed : IsClosed U)
-  (invariant : IsInvariant (fun n x ↦ f^[n] x) U)
-  (minimal : ∀ (x y : α) (_ : x ∈ U) (_ : y ∈ U) (ε : ℝ), ε > 0 → ∃ n : ℕ, f^[n] y ∈ ball x ε)
-
-/-- A dynamical system (α,f) is minimal if α is a minimal subset. -/
-def IsMinimal (f : α → α) : Prop := IsMinimalSubset f univ
-
-/-- In a minimal dynamics, the recurrent set is all the space. -/
-theorem recurrentSet_of_minimal_is_all_space (hf : IsMinimal f) (x : α) :
-    x ∈ recurrentSet f := by
-  have : ∀ (ε : ℝ) (N : ℕ), ε > 0 → ∃ m : ℕ, m ≥ N ∧ f^[m] x ∈ ball x ε := by
-    intro ε N hε
-    obtain ⟨n, hball⟩ : ∃ n, f^[n] (f^[N] x) ∈ ball x ε :=
-      hf.minimal x (f^[N] x) (mem_univ _) (mem_univ _) ε hε
-    exact ⟨n + N, by linarith, (Function.iterate_add_apply _ _ _ _).symm ▸ hball⟩
-  exact (recurrentSet_iff_accumulation_point f x).mpr this
-
 /-- The doubling map is the classic interval map -/
 noncomputable def doubling_map (x : unitInterval) : unitInterval :=
   ⟨Int.fract (2 * x), by exact unitInterval.fract_mem (2 * x)⟩
-
-/-- An example of a continuous dynamics on a compact space in which the recurrent set is all
-the space, but the dynamics is not minimal -/
-example : ¬IsMinimal (id : unitInterval → unitInterval) := by
-  intro H
-  have minimality := H.minimal 0 1 (mem_univ 0) (mem_univ 1)
-    ((dist (1 : unitInterval) (0 : unitInterval)) / 2)
-  revert minimality; contrapose!; intro _
-  -- we need this helper twice below
-  have dist_pos : 0 < dist (1 : unitInterval) 0 :=
-    dist_pos.mpr (unitInterval.coe_ne_zero.mp (by norm_num))
-  refine' ⟨div_pos dist_pos (by norm_num), fun n ↦ _⟩
-  simp only [iterate_id, id_eq, mem_ball, not_lt, half_le_self_iff]
-  exact le_of_lt dist_pos
-
-example (x : unitInterval) : x ∈ recurrentSet (id : unitInterval → unitInterval) :=
-  periodicpts_mem_recurrentSet _ _ 1 (by norm_num) (is_periodic_id 1 x)
-
-/-- Every point in a minimal subset is recurrent. -/
-theorem minimalSubset_mem_recurrentSet (U : Set α) (hU : IsMinimalSubset f U) :
-    U ⊆ recurrentSet f := by
-  intro x hx
-  obtain ⟨_, hInv, hMin⟩ := hU
-  apply (recurrentSet_iff_accumulation_point f x).mpr
-  intro ε N hε
-  obtain ⟨n, hball⟩ : ∃ n, f^[n] (f^[N] x) ∈ ball x ε :=
-    hMin x (f^[N] x) hx (Set.mapsTo'.mp (hInv N) ⟨x, hx, rfl⟩) ε hε
-  exact ⟨n + N, le_add_self, (Function.iterate_add_apply _ _ _ _).symm ▸ hball⟩
-
-/-- Every invariant nonempty closed subset contains at least a minimal invariant subset. -/
-theorem nonempty_invariant_closed_subset_has_minimalSubset
-    (U : Set α) (hne : Nonempty U) (hC : IsClosed U) (hI : IsInvariant (fun n x => f^[n] x) U) :
-    ∃ V : Set α, V ⊆ U → (hinv : MapsTo f U U) → IsMinimalSubset f U := by
-  -- This follows from Zorn's lemma
-  sorry
-
-/-- The recurrent set of `f` is nonempty -/
-theorem recurrentSet_nonempty [Nonempty α] : Set.Nonempty (recurrentSet f) := by
-  sorry
 
 end Topological_Dynamics

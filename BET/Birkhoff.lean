3f03f861bee781ebad008a083244c9b994e715a5
Marcello Seri <marcello.seri@gmail.com>
2024-05-17 16:19:15 +0200

    Birkhoff.lean: keep the sketch of the proof for reference
    Signed-off-by: Marcello Seri <marcello.seri@gmail.com>

--- BET/Birkhoff.lean
+++ BET/Birkhoff.lean
@@ -457,9 +457,43 @@ theorem integral_nonneg : 0 ≤ ∫ x in (divSet T φ), φ x ∂μ := by
   exact le_of_tendsto_of_tendsto' tendsto_const_nhds h2 h3
 
 
+-- Sketch of the proof of the main theorem, for reference
+
 /- `A` is in `I = inv_sigma_algebra`. -/
 -- idea: is it better to define a new type measureable sets in alpha and then restrict to that type?
 -- def inv_sigma_algebra := { S : Set α | MeasurableSet S ∧ T⁻¹' S = S }
 -- def inv_sigma_algebra := { S : Set α | MeasurableSet S ∧ IsInvariant (fun n x ↦ T^[n] x) S }
+
+/- define `Φ_n : max { ∑_{i=0}^{n} φ ∘ T^i }_{k ≤ n}` -/
+
+/- ∀ `x ∈ A`, `Φ_{n+1}(x) - Φ_{n}(T(x)) = φ(x) - min(0,Φ_{n}(T(x))) ≥ φ(x)` decreases to `φ(x)`. -/
+
+/- Using dominated convergence, `0 ≤ ∫_A (Φ_{n+1} - Φ_{n}) dμ = ∫_A (Φ_{n+1} - Φ_{n} ∘ T) dμ → ∫_A φ dμ`. -/
+
+/- `(1/n) ∑_{k=0}^{n-1} φ ∘ T^k ≤ Φ_n / n`. -/
+
+/- If `x ∉ A`, `limsup_n (1/n) ∑_{k=0}^{n-1} φ ∘ T^i ≤ 0`. -/
+
+/- If conditional expectation of `φ` is negative, i.e., `∫_C φ dμ = ∫_C φ|_inv_sigmal_algebra dμ < 0` for all `C` in `inv_sigma_algebra` with `μ(C) > 0`,
+then `μ(A) = 0`. -/
+
+/- Then (assumptions as prior step) `limsup_n (1/n) ∑_{k=0}^{n-1} φ ∘ T^i ≤ 0` a.e. -/
+
+/- Let `φ = f - f|_I - ε`. -/
+
+/- `f_I ∘ T = f|_I` and so `(1/n) ∑_{k=0}^{n-1} f ∘ T^k = (1/n) ∑_{k=0}^{n-1} f ∘ T^k - f_I - ε`. -/
+
+/- `limsup_n (1/n) ∑_{k=0}^{n-1} f ∘ T^i ≤ f|_I + ε` a.e. -/
+
+/- Replacing `f` with `-f`  we get the lower bound. -/
+
+/- Birkhoff's theorem: Almost surely, `birkhoffAverage ℝ f g n x` converges to the conditional expectation of `f`. -/
+
+/- If `T` is ergodic, show that the invariant sigma-algebra is a.e. trivial. -/
+
+/- Show that the conditional expectation with respect to an a.e. trivial subalgebra is a.e.
+the integral. -/
+
+/- Birkhoff theorem for ergodic maps. -/
 
 end Ergodic_Theory

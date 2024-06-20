5dfe9892159361a63ecd850d6fa457b23d1b74b3
Pietro Monticone <38562595+pitmonticone@users.noreply.github.com>
2024-06-20 16:16:53 +0200

    Update Subset.lean

--- BET/TopologicalEntropy/Systems/Subset.lean
+++ BET/TopologicalEntropy/Systems/Subset.lean
@@ -79,28 +79,22 @@ variable {X : Type _}
 
 theorem net_of_monotone_space {T : X → X} {F G : Set X} (F_sub_G : F ⊆ G ) {U : Set (X × X)} {n : ℕ}
     {s : Set X} (h : IsDynamicalNetOf T F U n s) :
-    IsDynamicalNetOf T G U n s :=
-  ⟨Set.Subset.trans h.1 F_sub_G, h.2⟩
+    IsDynamicalNetOf T G U n s := ⟨Set.Subset.trans h.1 F_sub_G, h.2⟩
 
 theorem net_maxcard_monotone_space (T : X → X) (U : Set (X × X)) (n : ℕ) :
     Monotone (fun F : Set X ↦ Maxcard T F U n) := by
   intro F G F_sub_G
   simp only
   rw [maxcard_eq_sSup T F U n, maxcard_eq_sSup T G U n]
-  apply sSup_le_sSup
-  apply Set.image_mono
-  apply Set.image_mono
-  rw [Set.setOf_subset_setOf]
-  intro _
-  exact net_of_monotone_space F_sub_G
+  apply sSup_le_sSup (Set.image_mono (Set.image_mono _))
+  exact fun _ ↦ net_of_monotone_space F_sub_G
 
 theorem net_entropy_monotone_space (T : X → X) (U : Set (X × X)) :
     Monotone (fun F : Set X ↦ NetEntropy T F U) := by
   intros F G F_sub_G
   apply Misc.EReal_liminf_le_liminf <| Filter.eventually_of_forall _
   intro n
-  apply EReal.div_left_mono _
-  apply log_monotone _
+  apply EReal.div_left_mono _ (log_monotone _)
   rw [ENat.toENNReal_le]
   exact net_maxcard_monotone_space T U n F_sub_G
 
@@ -109,22 +103,17 @@ theorem net_entropy'_monotone_space (T : X → X) (U : Set (X × X)) :
   intros F G F_sub_G
   apply Misc.EReal_limsup_le_limsup <| Filter.eventually_of_forall _
   intro n
-  apply EReal.div_left_mono _
-  apply log_monotone _
+  apply EReal.div_left_mono _ (log_monotone _)
   rw [ENat.toENNReal_le]
   exact net_maxcard_monotone_space T U n F_sub_G
 
 theorem entropy_monotone_space₂ [UniformSpace X] (T : X → X) :
-    Monotone (fun F : Set X ↦ Entropy T F) := by
-  intros F G F_sub_G
-  apply iSup₂_mono; intros U _
-  exact net_entropy_monotone_space T U F_sub_G
+    Monotone (fun F : Set X ↦ Entropy T F) :=
+  fun _ _ F_sub_G ↦ iSup₂_mono fun U _ ↦ net_entropy_monotone_space T U F_sub_G
 
 theorem entropy'_monotone_space₂ [UniformSpace X] (T : X → X)  :
-    Monotone (fun F : Set X ↦ Entropy' T F) := by
-  intros F G F_sub_G
-  apply iSup₂_mono; intros U _
-  exact net_entropy'_monotone_space T U F_sub_G
+    Monotone (fun F : Set X ↦ Entropy' T F) :=
+  fun _ _ F_sub_G ↦ iSup₂_mono fun U _ ↦ net_entropy'_monotone_space T U F_sub_G
 
 end EntropyMonotoneSpace
 
@@ -153,25 +142,21 @@ theorem dyncover_of_closure {F : Set X} {U V : Set (X × X)} (V_uni : V ∈ 𝓤
   use z
   apply And.intro z_in_s
   rw [mem_ball_symmetry (dynamical_of_symm_is_symm T W_symm n)] at y_in_ball_x
-  apply ball_mono (dynamical_of_comp_is_comp T U W n) z
-  exact mem_ball_comp y_in_ball_z y_in_ball_x
+  exact ball_mono (dynamical_of_comp_is_comp T U W n) z (mem_ball_comp y_in_ball_z y_in_ball_x)
 
 theorem cover_mincard_of_closure (F : Set X) (U : Set (X × X)) {V : Set (X × X)} (V_uni : V ∈ 𝓤 X)
     (n : ℕ) :
     Mincard T (closure F) (compRel U V) n ≤ Mincard T F U n := by
   rcases eq_top_or_lt_top (Mincard T F U n) with (mincard_infi | mincard_fin)
-  . rw [mincard_infi]
-    exact le_top
+  . exact mincard_infi ▸ le_top
   . rcases (finite_mincard_iff T F U n).1 mincard_fin with ⟨s, s_cover, s_mincard⟩
-    rw [← s_mincard]
-    exact mincard_le_card (dyncover_of_closure h V_uni s_cover)
+    exact s_mincard ▸ mincard_le_card (dyncover_of_closure h V_uni s_cover)
 
 theorem cover_entropy_of_closure (F : Set X) (U : Set (X × X)) {V : Set (X × X)} (V_uni : V ∈ 𝓤 X) :
     CoverEntropy T (closure F) (compRel U V) ≤ CoverEntropy T F U := by
   apply Misc.EReal_liminf_le_liminf <| Filter.eventually_of_forall _
   intro n
-  apply EReal.div_left_mono _
-  apply log_monotone _
+  apply EReal.div_left_mono _ (log_monotone _)
   rw [ENat.toENNReal_le]
   exact cover_mincard_of_closure h F U V_uni n
 
@@ -180,30 +165,25 @@ theorem cover_entropy'_of_closure (F : Set X) (U : Set (X × X)) {V : Set (X × 
     CoverEntropy' T (closure F) (compRel U V) ≤ CoverEntropy' T F U := by
   apply Misc.EReal_limsup_le_limsup <| Filter.eventually_of_forall _
   intro n
-  apply EReal.div_left_mono _
-  apply log_monotone _
+  apply EReal.div_left_mono _ (log_monotone _)
   rw [ENat.toENNReal_le]
   exact cover_mincard_of_closure h F U V_uni n
 
 theorem entropy_of_closure (F : Set X) :
     Entropy T (closure F) = Entropy T F := by
-  apply le_antisymm _ (EntropyMonotoneSpace.entropy_monotone_space₁ T (@subset_closure X F _))
-  apply iSup₂_le
+  apply le_antisymm (iSup₂_le _) (EntropyMonotoneSpace.entropy_monotone_space₁ T (@subset_closure X F _))
   intro U U_uni
   rcases comp_mem_uniformity_sets U_uni with ⟨V, V_uni, V_comp_U⟩
-  apply le_iSup₂_of_le V V_uni
-  apply le_trans (cover_entropy_antitone_uni T (closure F) V_comp_U)
-    (cover_entropy_of_closure h F V V_uni)
+  exact le_iSup₂_of_le V V_uni (le_trans (cover_entropy_antitone_uni T (closure F) V_comp_U)
+    (cover_entropy_of_closure h F V V_uni))
 
 theorem entropy'_of_closure (F : Set X) :
     Entropy' T (closure F) = Entropy' T F := by
-  apply le_antisymm _ (EntropyMonotoneSpace.entropy'_monotone_space₁ T (@subset_closure X F _))
-  apply iSup₂_le
+  apply le_antisymm (iSup₂_le _) (EntropyMonotoneSpace.entropy'_monotone_space₁ T (@subset_closure X F _))
   intro U U_uni
   rcases comp_mem_uniformity_sets U_uni with ⟨V, V_uni, V_comp_U⟩
-  apply le_iSup₂_of_le V V_uni
-  exact le_trans (cover_entropy'_antitone_uni T (closure F) V_comp_U)
-    (cover_entropy'_of_closure h F V V_uni)
+  exact le_iSup₂_of_le V V_uni (le_trans (cover_entropy'_antitone_uni T (closure F) V_comp_U)
+    (cover_entropy'_of_closure h F V V_uni))
 
 end EntropyClosure
 
